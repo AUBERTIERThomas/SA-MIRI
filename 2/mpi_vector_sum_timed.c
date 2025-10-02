0e52aece4726ded6e0e0e4177026f658290ba711
Kuba Seliga <kubaseli.02@gmail.com>
2025-10-02 22:01:31 +0200

    Pc

---
+++ 2/mpi_vector_sum_timed.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <sys/time.h> // for gettimeofday()
+
+#define N 256  // size of the vector
+
+int main(int argc, char** argv) {
+    int rank, size, i;
+    struct timeval start, end;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int local_n = N / size; // assume N divisible by number of processes
+
+    int* full_vector = NULL;
+    int* local_vector = malloc(local_n * sizeof(int));
+    int* result_vector = NULL;
+
+    if (rank == 0) {
+        full_vector = malloc(N * sizeof(int));
+        result_vector = malloc(N * sizeof(int));
+        for (i = 0; i < N; i++) {
+            full_vector[i] = i + 1; // example: [1, 2, 3, ..., N]
+        }
+        printf("Root process: initial vector = ");
+        for (i = 0; i < N; i++) printf("%d ", full_vector[i]);
+        printf("\n");
+    }
+
+    // Start timing
+    MPI_Barrier(MPI_COMM_WORLD); // ensure all processes start together
+    gettimeofday(&start, NULL);
+
+    // Distribute the vector among processes
+    MPI_Scatter(full_vector, local_n, MPI_INT,
+                local_vector, local_n, MPI_INT,
+                0, MPI_COMM_WORLD);
+
+    //printf("Process %d received elements:", rank);
+    for (i = 0; i < local_n; i++) printf(" %d", local_vector[i]);
+    printf("\n");
+
+    // Local computation: add 1 to each element
+    for (i = 0; i < local_n; i++) {
+        local_vector[i] += 1;
+    }
+
+    // Gather results to root process
+    MPI_Gather(local_vector, local_n, MPI_INT,
+               result_vector, local_n, MPI_INT,
+               0, MPI_COMM_WORLD);
+
+    // Stop timing
+    MPI_Barrier(MPI_COMM_WORLD);
+    gettimeofday(&end, NULL);
+
+    if (rank == 0) {
+        printf("Root process: gathered result = ");
+        for (i = 0; i < N; i++) printf("%d ", result_vector[i]);
+        printf("\n");
+
+        double elapsed = (end.tv_sec - start.tv_sec) * 1000.0 +
+                         (end.tv_usec - start.tv_usec) / 1000.0;
+        printf("Parallel execution time: %.2f ms\n", elapsed);
+    }
+
+    // Cleanup
+    if (rank == 0) {
+        free(full_vector);
+        free(result_vector);
+    }
+    free(local_vector);
+
+    MPI_Finalize();
+    return 0;
+}
--- 2/pi_MPI.c
+++ 2/pi_MPI.c
@@ -1,71 +1,50 @@
 #include <stdio.h>
 #include <mpi.h>
-#include <sys/time.h>
 
-#define N 1000000
-
-struct timeval s_time, e_time;
+#define N 100000000  // Total number of trapezoids
 
 double f(double x)
 {
-	return 4.0 / (1.0 + x*x);
+    return 4.0 / (1.0 + x * x);
 }
 
 double trap_rule(double a, double b, int n)
 {
-	double h = (b-a)/n;
-	double sum = (f(a) + f(b))/2.0;
-	
-	for(int i = 1; i < n; i++)
-		sum += f(a+i*h);
-	
-	return sum*h;
+    double h = (b - a) / n;
+    double sum = (f(a) + f(b)) / 2.0;
+
+    for (int i = 1; i < n; i++)
+        sum += f(a + i * h);
+
+    return sum * h;
 }
 
 int main(int argc, char **argv)
 {
-	int rank, size;
-	MPI_Init(NULL, NULL);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	
-	double a = 0.0, b = 1.0;
-	double local_a, local_b, h;
-	int local_n;
-	double local_result, total_result;
-	
-	int n = (N/size) * size;
-	h = (b-a)/n;
-	local_n = n/size;
-	local_a = a + rank*local_n*h;
-	local_b = a + (rank+1)*local_n*h;
-	
-	local_result = trap_rule(local_a, local_b, local_n);
-	
-	
-	if (rank == 0)
-	{
-		gettimeofday(&s_time, NULL);
-	
-		total_result = local_result;
-		for(int source = 1; source < size; source++)
-		{
-			double temp;
-			MPI_Recv(&temp, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			total_result += temp;
-		}
-		printf("Estimated PI = %.16f\n", total_result);
-		
-		gettimeofday(&e_time, NULL);
-		
-		int ttime = (e_time.tv_sec - s_time.tv_sec) * 1000000 + (e_time.tv_usec - s_time.tv_usec);
-		printf("It took %.6f s !\n", (float)ttime/1000000.0);
-	}
-	else
-	{
-		MPI_Send(&local_result, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-	}
-	
-	MPI_Finalize();
-	return 0;
+    int rank, size;
+    double local_a, local_b, local_integral;
+    int local_n;
+
+    double a = 0.0, b = 1.0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    double h = (b - a) / N;
+    local_n = N / size;
+    local_a = a + rank * local_n * h;
+    local_b = local_a + local_n * h;
+
+    local_integral = trap_rule(local_a, local_b, local_n);
+
+    double total_integral;
+    MPI_Reduce(&local_integral, &total_integral, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        printf("Estimated value of pi = %.15f\n", total_integral);
+    }
+
+    MPI_Finalize();
+    return 0;
 }
